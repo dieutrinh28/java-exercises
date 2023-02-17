9bed2adc8ad694c650dba21257e1e8c4234a0b5b
dieutrinh28 <trinhdo.work@gmail.com>
2023-02-17 11:36:59 +0700

    popup-menu

--- menu/app/src/main/java/com/example/menu/MainActivity.java
+++ menu/app/src/main/java/com/example/menu/MainActivity.java
@@ -8,14 +8,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
 
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_popup_menu = findViewById(R.id.btn_popup_menu);
+        btn_popup_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopupMenu(view);
+            }
+        });
     }
 
     @Override
@@ -42,4 +54,34 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+
+    private void showPopupMenu(View v) {
+        PopupMenu popup_menu = new PopupMenu(MainActivity.this, v);
+        popup_menu.getMenuInflater().inflate(R.menu.menu_popup, popup_menu.getMenu());
+        popup_menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                String message = menuItem.toString();
+                switch (menuItem.getItemId()) {
+                    case R.id.popup_reply:
+                    case R.id.popup_forward:
+                        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                        intent.putExtra("message", "You clicked menu item " + message);
+                        startActivity(intent);
+                        return true;
+                    default:
+                        return false;
+                }
+
+            }
+
+        });
+        popup_menu.show();
+    }
+
+
+
+
+
 }
